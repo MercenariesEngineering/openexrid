85ebd5bc7e1376bb7cadbe3b0bda0d842e6f7c32
Benjamin Legros <legros@mercs-eng.com>
2019-12-02 15:50:17 +0100

    [Build] Fixed CMakeLists.txt to not use conan by default

--- build-conan.sh
+++ build-conan.sh
@@ -14,7 +14,7 @@ cd build/release
 
 # CMake generate all makefiles
 echo "Building makefiles ..."
-cmake -G "Unix Makefiles" -D USE_CONAN=1 -D CMAKE_BUILD_TYPE=Release -D CMAKE_C_COMPILER=$CC_VER -D CMAKE_CXX_COMPILER=$CXX_VER ../../
+cmake -G "Unix Makefiles" -D USE_CONAN=1 -D CONAN_BUILD_INFO_DIR=Conan -D CMAKE_BUILD_TYPE=Release -D CMAKE_C_COMPILER=$CC_VER -D CMAKE_CXX_COMPILER=$CXX_VER ../../
 
 # And make
 echo "Make ..."
--- linux-docker-build.sh
+++ linux-docker-build.sh
@@ -10,13 +10,15 @@ mkdir -p conan_data
 # Build the docker image
 docker build -t "linux-openexrid-builder" "$this_directory/docker" --rm
 
+nukes=""
+for nuke in `ls -d /usr/local/Nuke*` ; do
+	nukes="$nukes -v $nuke:$nuke"
+done
+
 docker run --rm -it \
 	-u $(id -u):$(id -g) \
 	-v "$this_directory:/openexrid" \
 	-v "$this_directory/conan_data:/conan/.conan/data" \
 	-v "$this_directory/../openfx:/usr/local/openfx" \
-	-v "/usr/local/Nuke10.5v8:/usr/local/Nuke10.5" \
-	-v "/usr/local/Nuke11.2v7:/usr/local/Nuke11.2" \
-	-v "/usr/local/Nuke11.3v5:/usr/local/Nuke11.3" \
-	-v "/usr/local/Nuke12.0v3:/usr/local/Nuke12.0" \
+	$nukes \
 	"linux-openexrid-builder" ./build-conan.sh
